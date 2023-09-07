0ec2147f240295346e12af59395da4bec4d0a858
DLIN131 <daiyulin945@gmail.com>
2023-09-08 00:08:13 +0800

    re deploy

--- assets/downloadPage-060a6544.js
+++
@@ -1,1 +0,0 @@
-import{a4 as e,l as o,m as a}from"./index-281d742b.js";const n={};function c(t,r){return o(),a("div",null," this is download page ")}const l=e(n,[["render",c]]);export{l as default};
